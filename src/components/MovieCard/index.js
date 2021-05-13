ecdd5bd1c83e9ea9612feaa93e25929b35f2e917
quangnt-itman <quangnt.itman@gmail.com>
2021-05-13 21:05:29 +0700

    Implement Component MovieCard && style

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -4,9 +4,32 @@ import { connect } from 'react-redux';
 export class MovieCard extends Component {
   render () {
     return (
-      <div>
-        <h3>MovieCard</h3>
-      </div>
+      <>
+        {/* <h3>MovieCard</h3> */ }
+        <div className="movie-card w-25" >
+          <div className="card" >
+            <aside>
+              <div>
+                <a
+                  type="button"
+                  className=" play "
+                  data-toggle="modal"
+                  data-target="#movie-card-modal"
+                >
+                  <i className="material-icons" >play_arrow</i>
+                  <a href="#" className="read-more">read more</a>
+                </a>
+              </div>
+            </aside>
+            <div className="card-img">
+              <img src="./assets/images/movie-1.jpg" className="card-img-top" alt="" />
+            </div>
+            <div className="card-body">
+              <p className="card-text">movie name</p>
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
