a9496733e852895c5fa45d1e8629786858a97d9e
quangnt-itman <quangnt.itman@gmail.com>
2021-05-29 04:51:19 +0700

    Xong Phan API cua cinemas

--- src/components/Cinemas/PhimCard.js
+++ src/components/Cinemas/PhimCard.js
@@ -10,11 +10,11 @@ export class PhimCard extends Component {
           <div className="card mb-3" style={ { maxWidth: 540 } }>
             <div className="row no-gutters">
               <div className="col-md-4">
-                <img src={ phim.hinhAnh } className="card-img" alt={phim.maPhim } />
+                <img src={ phim.hinhAnh } className="card-img" alt={ phim.maPhim } />
               </div>
               <div className="col-md-8">
                 <div className="card-body">
-                  <h5 className="card-title">{ phim.tenPhim gi}</h5>
+                  <h5 className="card-title">{ phim.tenPhim }</h5>
                   <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                   <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                 </div>
